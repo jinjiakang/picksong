0bb6ff0d5130db20f82c093fc281599a8ed50d57
erickang <10346012@ntub.edu.tw>
2018-07-04 10:15:18 +0000

    add random song and fix import bug

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,20 @@
 module PagesHelper
+    def rand_id
+        @rnumber = rand(Songlist.first.id .. Songlist.last.id)
+        @right = false 
+        until @right == true
+            @right = true
+            begin
+              Songlist.find(@rnumber)
+            rescue
+              @rnumber = rand(1..10)
+              @right = false
+            end
+        end
+        return @rnumber
+    end
+    def embed_url(url)
+        @i = url.index("=", 1) +1
+        return url[@i..-1]
+    end
 end
